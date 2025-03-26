535318e649e652b8a58f3558fbb89113ce53a3b7
Arianto Zaki Hamdalah <arianto.zaki.hamdalah.03@gmail.com>
2025-03-26 10:02:07 +0700

    minor-update: make it clean code

--- lib/html_storage.dart
+++ lib/html_storage.dart
@@ -3,11 +3,7 @@ import 'dart:convert';
 // ignore: avoid_web_libraries_in_flutter, deprecated_member_use
 import 'dart:html' as html;
 
-import 'package:flutter/cupertino.dart';
-
-class _ValueStorage<T> extends ValueNotifier<T> {
-  _ValueStorage(super.value);
-}
+import 'package:flutter/foundation.dart';
 
 @protected
 class Storage {
@@ -26,12 +22,8 @@ class Storage {
 
   static Future<void> init([Map<String, dynamic>? initialData]) async {
     _subject.value = initialData ?? <String, dynamic>{};
-    if (await _exists()) {
-      await _readFromStorage();
-    } else {
-      await _writeToStorage(_subject.value);
-    }
-    return;
+    if (await _exists()) return await _readFromStorage();
+    return await _writeToStorage(_subject.value);
   }
 
   static T? read<T>(String key) => _subject.value[key] as T?;
@@ -42,19 +34,14 @@ class Storage {
     _subject.value.clear();
   }
 
-  static Future<void> flush() => _writeToStorage(_subject.value);
-
   static html.Storage get _localStorage => html.window.localStorage;
   static Future<bool> _exists() async => _localStorage.containsKey(_fileName);
   static Future<void> _writeToStorage(Map<String, dynamic> data) async =>
       _localStorage.update(_fileName, (val) => json.encode(data), ifAbsent: () => json.encode(_subject.value));
   static Future<void> _readFromStorage() async {
     final dataFromLocal = _localStorage.entries.firstWhereOrNull((value) => value.key == _fileName);
-    if (dataFromLocal != null) {
-      _subject.value = json.decode(dataFromLocal.value) as Map<String, dynamic>;
-    } else {
-      await _writeToStorage(<String, dynamic>{});
-    }
+    if (dataFromLocal == null) return await _writeToStorage(<String, dynamic>{});
+    _subject.value = json.decode(dataFromLocal.value) as Map<String, dynamic>;
   }
 }
 
@@ -65,4 +52,8 @@ extension FirstWhereExt<T> on Iterable<T> {
     }
     return null;
   }
+}
+
+class _ValueStorage<T> extends ValueNotifier<T> {
+  _ValueStorage(super.value);
 }
